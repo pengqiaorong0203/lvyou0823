e90c73851a18f9e7eff127488918c2e5b2e0de9c
Aronger123 <821779094@qq.com>
2019-08-23 15:05:20 +0800

    Insert fail

--- src/main/java/com/newer/lvyou/Application.java
+++ src/main/java/com/newer/lvyou/Application.java
@@ -9,7 +9,7 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-        //啊哈哈哈          ----彭俏蓉
+        //啊哈哈哈      tent吧vv旅人我v人    ----彭俏蓉
     }
     //我是胡
     //fghjk--hjw
