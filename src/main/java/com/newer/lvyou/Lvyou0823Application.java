585f585a81fea8fae595e293784810d9881bd7a7
Aronger123 <821779094@qq.com>
2019-08-23 14:02:45 +0800

    第二个项目旅游

---
+++ src/main/java/com/newer/lvyou/Lvyou0823Application.java
@@ -0,0 +1,13 @@
+package com.newer.lvyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lvyou0823Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lvyou0823Application.class, args);
+    }
+
+}
