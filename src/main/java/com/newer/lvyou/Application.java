b0ba0a211decf17831e95ca220882caee2fea763
hu8681912 <1034868569@qq.com>
2019-08-24 10:01:00 +0800

    8-24早  添加Application中的注解

--- src/main/java/com/newer/lvyou/Application.java
+++ src/main/java/com/newer/lvyou/Application.java
@@ -1,9 +1,13 @@
 package com.newer.lvyou;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@MapperScan("com.newer.lvyou.mapper")
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
